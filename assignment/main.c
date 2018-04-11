4033a1dac17eec9d383bf89e3e6d7da61a813826
Kiweewa-Ashiraph <38238519+Kiweewa-Ashiraph@users.noreply.github.com>
2018-04-11 16:07:21 -0400

    question 1
    error correction from aprogramm

---
+++ assignment/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int x=1;//it is not : but we use ;
+
+int main()
+{ if(x==1)// there is no termimantion and its == but not =
+   {
+
+    printf(" x equals 1");
+   }else{//not otherwise
+       printf(" x does not equal 1");
+
+   }
+    return 0;
+}
