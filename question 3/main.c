71e07bca766d04fddaa5eb93cb9afc0367b85273
Kiweewa-Ashiraph <38238519+Kiweewa-Ashiraph@users.noreply.github.com>
2018-04-11 16:36:48 -0400

    QUESTION 3 and QUESTION 4 1st part array cording
    array statements of question three and question 4 saved as qn3.d

---
+++ question 3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     long x[50];//an array x containing 50 elements
+     x[49] =123.456;//an array at the 50th position is 123.456
+
+
+    return 0;
+}
---
+++ question 3/qn3 pat c 2nd part/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x;
+    for(x=0;x<100;x++)
+    {
+
+
+    printf("%d\n",x);}
+    //x is equal to x={0 up to 99}
+    return 0;
+}
