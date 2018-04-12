6dce8cdb7da66191f5fb376736d577054f36e533
Kiweewa-Ashiraph <38238519+Kiweewa-Ashiraph@users.noreply.github.com>
2018-04-12 00:53:06 -0400

    Add files via upload

---
+++ assgn4/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int *add_array(int array_1[3],int array_2[3]), i;
+main();
+{
+    int array_1[] = {2,5,3,22,6};
+    int array_2[] ={13,143,11,10,121};
+
+    int *array_3 = add_array(array_1,array_2);
+    for( i=0;i<3;i++)
+    {
+         printf("%d \n",array_3[1]);
+    }
+    }
+    int *add_array( int array_1[],int array_2[],int length)
+    (
+      int *destination_of_the_array = malloc(length * sizeof(int));
+        for(int i=0;i< lenghth; i++){
+            destination_of_the_array[i] =array_1[i] + array_2[i];
+
+    }
+
+    return destination_of_the_array;
+
+    return 0;
